06852a45b88c446f41fc719b5763b521ecd8e877
NJIB <njibuck@gmail.com>
2019-12-13 22:12:06 -0500

    Basic formatting; POST and GETs working; Delete functionality in but not working correctly

--- public/app.js
+++ public/app.js
@@ -11,6 +11,21 @@ $.getJSON('/api/articles', function(data) {
   }
 });
 
+// Click handler for the Submit button
+// $(document).on('click', '#submit', function() {
+
+// console.log("Scrape request made...");
+
+//   $.ajax({
+//     method: 'GET',
+//     url: '/scrape'
+//   })
+//   .then(function(data) {
+//     console.log(data);
+//   });
+// });
+
+
 
 // Whenever someone clicks a p tag
 $(document).on('click', 'p', function() {
@@ -35,6 +50,7 @@ $(document).on('click', 'p', function() {
         $('#notes').append('<textarea id=\'bodyinput\' name=\'body\'></textarea>');
         // A button to submit a new note, with the id of the article saved to it
         $('#notes').append('<button data-id=\'' + data._id + '\' id=\'savenote\'>Save Note</button>');
+        $('#notes').append('<button data-id=\'' + data._id + '\' id=\'deletenote\'>Delete Note</button>');
 
         // If there's a note in the article
         if (data.note) {
@@ -61,6 +77,29 @@ $(document).on('click', '#savenote', function() {
       // Value taken from note textarea
       body: $('#bodyinput').val(),
     },
+  })
+  // With that done
+      .then(function(data) {
+      // Log the response
+        console.log(data);
+        // Empty the notes section
+        $('#notes').empty();
+      });
+
+  // Also, remove the values entered in the input and textarea for note entry
+  $('#titleinput').val('');
+  $('#bodyinput').val('');
+});
+
+// When you click the deletenote button
+$(document).on('click', '#deletenote', function() {
+  // Grab the id associated with the article from the submit button
+  const thisId = $(this).attr('data-id');
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: 'GET',
+    url: '/api/notes/' + thisId,
   })
   // With that done
       .then(function(data) {
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const cheerio = require('cheerio');
 // Require all models
 const db = require('./models');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 
 // Initialize Express
 const app = express();
@@ -27,30 +27,43 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Connect to the Mongo DB
-mongoose.connect('mongodb://localhost/Scraper', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect('mongodb://localhost/Scraper2', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 // Routes
 
 // A GET route for scraping the BBC website
 app.get('/scrape', async function (req, res) {
   // First, we grab the body of the html with axios
-  // const response = await axios.get('http://www.echojs.com/');
 
+  console.log("SCRAPING!!!");
+
+  // const response = await axios.get('https://www.bbc.com/sport/formula1');
   const response = await axios.get('https://www.bbc.com/sport/football');
   // Then, we load that into cheerio and save it to $ for a shorthand selector
   const $ = cheerio.load(response.data);
 
   // Now, we grab every h2 within an article tag, and do the following:
+  // $('h3.lakeside__title').each(function (i, element) {
     $('h3.gs-c-promo-heading__title').each(function (i, element) {
-            // Save an empty result object
-            const result = {};
+    // Save an empty result object
+    const result = {};
+
+    // console.log("(this).text :", $(this).text());
 
     // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .text();
-      result.link = $(this)
-        .closest('a')
-        .attr('href');
+    result.title = $(this)
+      .text();
+    result.link = $(this)
+      .closest('a')
+      .attr('href');
+
+    // Add the text and href of every link, and save them as properties of the result object
+    // result.title = $(this)
+    //   .text();
+    // result.link = $(this)
+    //   .children('a')
+    //   .attr('href');
+
 
 
     // Create a new Article using the `result` object built from scraping
@@ -63,9 +76,10 @@ app.get('/scrape', async function (req, res) {
         // If an error occurred, log it
         console.log(err);
       });
-  });
 
-  console.log(result);
+    console.log(result);
+
+  });
 
   // Send a message to the client
   res.send('Scrape Complete');
@@ -91,43 +105,57 @@ app.get('/api/articles/:id', async function (req, res) {
   // Finish the route so it finds one article using the req.params.id,
   // and run the populate method with "note",
   // then responds with the article with the note included
-  const id = req.params.id; 
+  const id = req.params.id;
   try {
     const data = await db.Article.findOne({
       //If used .find, will return an array (and data returned would need to be indexed (e.g. data[0], as shown below))
       _id: id
     })
-    .populate('note');
+      .populate('note');
     res.json(data);
   } catch (err) {
-    res.status(500).json({error: {name: err.name, message: err.message}});
+    res.status(500).json({ error: { name: err.name, message: err.message } });
   }
 });
 
 // Route for saving/updating an Article's associated Note
-// app.post('/api/articles/:articleid', async function (req, res) {
-  app.post('/api/articles/:id', async function (req, res) {
-    // TODO
+app.post('/api/articles/:id', async function (req, res) {
+  // TODO
   // ====
   // save the new note that gets posted to the Notes collection
   // then find an article from the req.params.id
   // and update it's "note" property with the _id of the new note
-    // then responds with the article with the note included
-    try {
-      const dbNote = await db.Note.create(req.body);
+  // then responds with the article with the note included
+  try {
+    const dbNote = await db.Note.create(req.body);
 
-      const dbArticle = await db.Article.findOneAndUpdate(
-        // {_id: req.params.articleid},
-        {_id: req.params.id},
-        {note: dbNote._id},
-        {new: true}
-      )
+    const dbArticle = await db.Article.findOneAndUpdate(
+      { _id: req.params.id },
+      { note: dbNote._id },
+      { new: true }
+    )
 
-      res.json(dbArticle);
-    } catch (err) {
-      res.status(500).json({error: {name: err.name, message: err.message}});
-    }  
+    res.json(dbArticle);
+  } catch (err) {
+    res.status(500).json({ error: { name: err.name, message: err.message } });
+  }
 });
+
+// Route for deleting a specific Note by id
+app.get('/api/notes/:id', async function (req, res) {
+  const id = req.params.id;
+  try {
+    const data = await db.Note.remove({
+      //If used .find, will return an array (and data returned would need to be indexed (e.g. data[0], as shown below))
+      _id: id
+    })
+      // .populate('note');
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: { name: err.name, message: err.message } });
+  }
+});
+
 
 // Set the app to listen on PORT
 app.listen(PORT, function () {
